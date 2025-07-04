6bf59dd995e8b95a7cb50585351d1043830ee427
Michael <coalvalleytech@gmail.com>
2025-07-04 14:12:12 -0400

    Add socials

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -36,6 +36,7 @@ const Header = () => {
         </nav>
         {/* Social Icons */}
         <div className="flex space-x-4">
+          {/* Facebook - outlined modern icon */}
           <a
             href="https://facebook.com"
             target="_blank"
@@ -43,11 +44,12 @@ const Header = () => {
             aria-label="Facebook"
             className="hover:text-secondary-300 text-cream-100 transition-colors"
           >
-            {/* Facebook Official SVG */}
-            <svg className="w-8 h-8" viewBox="0 0 320 512" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path d="M279.14 288l14.22-92.66h-88.91V127.91c0-25.35 12.42-50.06 52.24-50.06H293V6.26S259.5 0 225.36 0c-73.22 0-121 44.38-121 124.72v70.62H22.89V288h81.47v224h100.2V288z"/>
+            <svg className="w-7 h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+              <rect x="2" y="2" width="20" height="20" rx="6" stroke="currentColor" strokeWidth="1.5" fill="none" />
+              <path d="M15 8.5h-1.5V7.5c0-.28.22-.5.5-.5H15V5h-1c-1.38 0-2.5 1.12-2.5 2.5V8.5H10V11h1.5v5H13v-5h1.25l.25-2.5z" stroke="currentColor" strokeWidth="1.5" fill="none" />
             </svg>
           </a>
+          {/* Instagram - outlined modern icon */}
           <a
             href="https://instagram.com"
             target="_blank"
@@ -55,21 +57,23 @@ const Header = () => {
             aria-label="Instagram"
             className="hover:text-secondary-300 text-cream-100 transition-colors"
           >
-            {/* Instagram Official SVG */}
-            <svg className="w-8 h-8" viewBox="0 0 448 512" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9 114.9-51.3 114.9-114.9S287.7 141 224.1 141zm0 186c-39.5 0-71.5-32-71.5-71.5s32-71.5 71.5-71.5 71.5 32 71.5 71.5-32 71.5-71.5 71.5zm146.4-194.3c0 14.9-12 26.9-26.9 26.9s-26.9-12-26.9-26.9 12-26.9 26.9-26.9 26.9 12 26.9 26.9zm76.1 27.2c-1.7-35.3-9.9-66.7-36.2-92.1S388.6 1.7 353.3 0C317.7-1.7 130.3-1.7 94.7 0 59.4 1.7 28 9.9 2.6 36.2S1.7 59.4 0 94.7C-1.7 130.3-1.7 317.7 0 353.3c1.7 35.3 9.9 66.7 36.2 92.1s56.8 34.5 92.1 36.2c35.6 1.7 223 1.7 258.6 0 35.3-1.7 66.7-9.9 92.1-36.2s34.5-56.8 36.2-92.1c1.7-35.6 1.7-223 0-258.6zM398.8 388c-7.8 19.6-22.9 34.7-42.5 42.5-29.4 11.7-99.2 9-132.3 9s-102.9 2.6-132.3-9c-19.6-7.8-34.7-22.9-42.5-42.5-11.7-29.4-9-99.2-9-132.3s-2.6-102.9 9-132.3c7.8-19.6 22.9-34.7 42.5-42.5C121.1 9 190.9 11.6 224 11.6s102.9-2.6 132.3 9c19.6 7.8 34.7 22.9 42.5 42.5 11.7 29.4 9 99.2 9 132.3s2.7 102.9-9 132.3z"/>
+            <svg className="w-7 h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+              <rect x="3" y="3" width="18" height="18" rx="5" stroke="currentColor" strokeWidth="1.5" fill="none" />
+              <circle cx="12" cy="12" r="5" stroke="currentColor" strokeWidth="1.5" fill="none" />
+              <circle cx="17" cy="7" r="1.2" stroke="currentColor" strokeWidth="1.5" fill="none" />
             </svg>
           </a>
+          {/* Twitter - outlined modern icon */}
           <a
-            href="https://x.com"
+            href="https://twitter.com"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="X"
+            aria-label="Twitter"
             className="hover:text-secondary-300 text-cream-100 transition-colors"
           >
-            {/* X (Twitter) Official SVG */}
-            <svg className="w-8 h-8" viewBox="0 0 512 512" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path d="M459.4 151.7c.3 2.8.3 5.7.3 8.5 0 86.9-66.2 187.1-187.1 187.1-37.2 0-71.9-10.9-101.1-29.7 5.3.6 10.4.9 15.8.9 30.9 0 59.4-10.5 82-28.1-29-1-53.4-19.6-61.8-45.7 4.1.6 8.1 1.2 12.5 1.2 6 0 12-.8 17.6-2.3-30.2-6.1-53-32.6-53-64.6v-.8c8.9 4.9 19.1 7.9 29.9 8.3-17.8-11.9-29.5-32.1-29.5-55.1 0-12.1 3.2-23.4 8.9-33.1 32.4 39.8 80.8 65.9 135.3 68.7-1.1-4.8-1.7-9.8-1.7-15 0-36.1 29.3-65.4 65.4-65.4 18.8 0 35.8 7.9 47.7 20.7 14.9-2.8 29-8.3 41.6-15.8-4.9 15.3-15.3 28.1-29 36.2 13.2-1.4 25.9-5.1 37.6-10.2-8.9 13.1-20 24.7-32.9 34z"/>
+            <svg className="w-7 h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+              <rect x="2" y="2" width="20" height="20" rx="6" stroke="currentColor" strokeWidth="1.5" fill="none" />
+              <path d="M8 15c6 0 9-5 9-9 0-.14 0-.28-.01-.42A6.48 6.48 0 0 0 19 4.5a6.36 6.36 0 0 1-1.89.52A3.28 3.28 0 0 0 18.5 3.5a6.56 6.56 0 0 1-2.08.8A3.28 3.28 0 0 0 12 8.28c0 .26.03.52.08.76A9.32 9.32 0 0 1 5 4.5s-4 9 5 13c-1.38.94-3.13 1.5-5 1.5.65 0 1.28-.07 1.89-.21A9.29 9.29 0 0 0 12 21c7.5 0 11.5-6.22 11.5-11.62 0-.18-.01-.36-.02-.54A8.18 8.18 0 0 0 22 5.92a8.36 8.36 0 0 1-2.36.65A4.13 4.13 0 0 0 22 4.5z" stroke="currentColor" strokeWidth="1.5" fill="none" />
             </svg>
           </a>
         </div>
