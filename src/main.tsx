fe171c8534068c8922bfecd2d4554ca5282bbbe8
Michael <coalvalleytech@gmail.com>
2025-07-06 20:30:49 -0400

    Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
@@ -23,6 +25,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       {/* BrowserRouter enables client-side routing */}
       <BrowserRouter>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
